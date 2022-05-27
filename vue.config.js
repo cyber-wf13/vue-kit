f68e8558f5c1250921ffd54d0143a5973fd543fd
cyber-wf13 <o.zon77788@gmail.com>
2022-05-27 07:47:51 +0300

    Add base style rules

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "~@/scss/style.scss";`,
+      },
+    },
+  },
 });
