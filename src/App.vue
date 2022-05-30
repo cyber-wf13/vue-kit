a74e0855886eba0bafd09e9e4344ee5e859d2a85
cyber-wf13 <o.zon77788@gmail.com>
2022-05-30 19:08:00 +0300

    Add checkbox, radio and toggle inputs

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,146 @@
 <template>
-<div>
-  <test-test></test-test>
-</div>
+  <section class="basic">
+    <h2 class="basic__title title">Inputs</h2>
+    <div class="basic__content">
+      <div class="basic__block basic-block">
+        <h4 class="basic-block__title">Checkboxes</h4>
+        <div class="basic-block__body">
+          <radio-check
+            @change="radioCheckHandle"
+            v-for="checkbox in checkboxes"
+            :key="checkbox.id"
+            :value="checkbox.value"
+            :type="checkbox.type"
+            :name="checkbox.name"
+            >RadioCheck {{ checkbox.id }}
+          </radio-check>
+        </div>
+      </div>
+
+      <div class="basic__block basic-block">
+        <h4 class="basic-block__title">Radios</h4>
+        <div class="basic-block__body">
+          <radio-check
+            @change="radioCheckHandle"
+            v-for="radio in radios"
+            :key="radio.id"
+            :value="radio.value"
+            :type="radio.type"
+            :name="radio.name"
+            class="toggle"
+            >RadioCheck {{ radio.id }}
+          </radio-check>
+        </div>
+      </div>
+
+      <div class="basic__block basic-block">
+        <h4 class="basic-block__title">Toggle Buttons</h4>
+        <div class="basic-block__body">
+          <radio-check
+            @change="radioCheckHandle"
+            v-for="checkbox in checkboxes"
+            :key="checkbox.id"
+            :value="checkbox.value"
+            :type="checkbox.type"
+            :name="checkbox.name"
+            class="toggle"
+            >RadioCheck {{ checkbox.id }}
+          </radio-check>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import RadioCheck from "@/components/RadioCheck.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      checkboxes: [
+        {
+          id: 0,
+          type: "checkbox",
+          name: "RadioCheckName-1",
+          value: "CheckBox-1",
+        },
+        {
+          id: 1,
+          type: "checkbox",
+          name: "RadioCheckName-2",
+          value: "CheckBox-2",
+        },
+        {
+          id: 2,
+          type: "checkbox",
+          name: "RadioCheckName-3",
+          value: "CheckBox-3",
+        },
+      ],
+      radios: [
+        {
+          id: 0,
+          type: "radio",
+          name: "RadioCheckName",
+          value: "Radio-1",
+        },
+        {
+          id: 1,
+          type: "radio",
+          name: "RadioCheckName",
+          value: "Radio-1",
+        },
+        {
+          id: 2,
+          type: "radio",
+          name: "RadioCheckName",
+          value: "Radio-3",
+        },
+      ],
+    };
+  },
   components: {
+    RadioCheck,
+  },
+  methods: {
+    radioCheckHandle(e) {
+      console.log("From component", e.target.value);
+    },
   },
 };
 </script>
 
 <style lang="scss">
+@import "@/scss/style";
 
+.title {
+  font-size: rem(21);
+  font-weight: 400;
+  line-height: rem(30);
+}
+
+.basic {
+  &__content {
+    display: flex;
+    margin: 10px 0;
+  }
+}
+
+.basic-block {
+  flex: 1 0 25%;
+  &__title {
+    font-size: rem(16);
+    font-weight: 700;
+    line-height: rem(27);
+  }
+
+  &__body {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 5px 0;
+  }
+}
 </style>
